4dfd1de878b77ae442e5581d6749c5e7117475f3
maveg <maveg@MSI>
2023-08-28 21:58:28 -0400

    - added create_list function

--- invoice_table_data.py
+++ invoice_table_data.py
@@ -1,2 +1,19 @@
-def invoice_start_message():
-    return "This code is being executed from a python function within another file"
+def create_list():
+    """
+    Creates a list of invoice numbers based on user input.
+    
+    Returns:
+    invoice_list (list): List of entered invoice numbers.
+    """
+    invoice_list = []
+    while True:
+        user_input = input("Enter the Receipt # or (type 'exit' to quit):")
+        if user_input.lower() == "exit":
+            break
+        invoice_list.append(user_input)
+    return invoice_list
+
+if __name__ == "__main__":
+    # Call the create_list function to gather invoice numbers from the user
+    my_list = create_list()
+    print("Created List:", my_list)
